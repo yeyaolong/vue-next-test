f68a64a9b52cbabd0774441ba6dd75040b08562b
Hinanawi Tenshi <yeyaolong@users.noreply.github.com>
2020-05-26 16:20:35 +0800

    eslint规则修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
-    'no-unused-labels': 'off'
+    'no-unused-labels': 'off',
+    ' vue/no-unused-components': 'off'
   }
 }
